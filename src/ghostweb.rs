08666a790dfec7f3c55c2de4c0a7067604606c73
Ulrich Berthold <u@sansculotte.net>
2021-11-18 06:26:49 +0100

    pass previous points as parameters and add 2 new functions

--- src/ghostweb.rs
+++ src/ghostweb.rs
@@ -115,12 +115,10 @@ pub fn ghostweb(
             state.sample = 0.;
         }
 
-        state = advance(state, &params);
+        state = advance(i, state, &params);
 
-        state.i = i;
-        state.r = radius; // * state.n; //(state.n + params.rms);
-        state.p1 = equation_1(&state, &params);
-        state.p2 = equation_2(&state, &params);
+        state.p1 = equation_1(&state, &params, &state.p1, &state.p2);
+        state.p2 = equation_2(&state, &params, &state.p2, &state.p1);
 
         xs.push(Feed {
             x1: state.p1.x,
@@ -135,31 +133,34 @@ pub fn ghostweb(
     xs
 }
 
-fn advance(mut state: State, p: &Parameter) -> State {
-    state.c = (state.i as f64 / p.iterations as f64) * PI * 2.0;
+fn advance(i: u32, mut state: State, p: &Parameter) -> State {
+    let part = i as f64 / p.iterations as f64;
+    state.i = i;
+    state.c =  part * PI * 2.0;
     state.c2 = state.c * E;
     state.c3 = state.c * PHI;
 
     state.rf = state.c / 2. + 0.15;
     state.n = state.rf * p.m * (1. - p.m);
+    state.r -= p.radius as f64 / p.iterations as f64;
     state
 }
 
-fn equation_000(s: &State, p: &Parameter) -> Point {
+fn equation_000(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
     let x: f64 = s.c.sin();
     let y: f64 = s.c.cos();
-    let z: f64 = s.p1.z;
+    let z: f64 = s.sample;
     Point { x: x, y: y, z: z }
 }
 
-fn equation_001(s: &State, p: &Parameter) -> Point {
+fn equation_001(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
     let x: f64 = s.c.sin();
     let y: f64 = (x.powf(3.) + 0.5 * x + 0.3333).sqrt();
-    let z: f64 = s.p1.z;
+    let z: f64 = p2.z;
     Point { x: x, y: y, z: z }
 }
 
-fn equation_002(s: &State, p: &Parameter) -> Point {
+fn equation_002(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
     let r = s.c / (2. * PI);
     let x: f64 = s.c.cos() * r;
     let y: f64 = s.c.sin() * r;
@@ -167,37 +168,60 @@ fn equation_002(s: &State, p: &Parameter) -> Point {
     Point { x: x, y: y, z: z }
 }
 
-fn equation_003(s: &State, p: &Parameter) -> Point {
-    let x: f64 = (p.t + s.c * s.p2.z).sin()
+fn equation_003(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
+    let x: f64 = (p.t + s.c * p2.z).sin()
         * (s.c2 * p.t.powf(s.c3)).cos()
-        * s.hbm.get([s.p2.x, s.p2.y, s.p2.z]);
+        * s.hbm.get([p2.x, p2.y, p2.z]);
     let y: f64 = (p.t * 4000. + s.c).sin() * (p.rms - p.t.powf(s.sample as f64)).sin();
-    let z: f64 = s.sample * s.osx.get([s.p1.x, s.p1.y, p.t]);
+    let z: f64 = s.sample * s.osx.get([p1.x, p1.y, p.t]);
     Point { x: x, y: y, z: z }
 }
 
-fn equation_004(s: &State, p: &Parameter) -> Point {
-    let x = ((s.c2 + p.t) + s.p1.z + s.n).sin();
-    let y = (s.c3 + p.t).cos() * s.billow.get([s.p1.x, s.p1.x, p.t * 2000.]);
+fn equation_004(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
+    let x = ((s.c2 + p.t) + p1.z + s.n).sin();
+    let y = (s.c3 + p.t).cos() * s.billow.get([p1.x, p1.y, p.t * 2000.]);
     let z = (s.sample * p.rms + p.t) * s.c;
     Point { x: x, y: y, z: z }
 }
 
-fn equation_005(s: &State, p: &Parameter) -> Point {
+fn equation_005(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
     let x = (s.c3 * s.r * s.n).sin() + (s.c * p.t - s.c2).cos();
     let y = s.c3.cos() * (s.n * p.t + p.rms).cos() + (p.t + (s.sample as f64).powf(E)).sin();
-    let z = s.hbm.get([s.p1.x, s.p1.y, s.sample as f64])
-        + s.billow.get([s.p2.x, s.p2.y, s.p2.z]) * s.sample;
+    let z = s.hbm.get([p1.x, p1.y, s.sample as f64])
+        + s.billow.get([p2.x, p2.y, p2.z]) * s.sample;
     Point { x: x, y: y, z: z }
 }
 
-fn select_equation(index: usize) -> fn(&State, &Parameter) -> Point {
+fn equation_006(s: &State, p: &Parameter, p1: &Point, p2: &Point) -> Point {
+    let mut x = s.c.sin() + (p2.z.abs() + 0.01).ln() * s.c;
+    let mut y = (s.c.cos() + (s.c2 * p1.z).sin() + (x + p1.z).powf(s.c3) * s.r).atan() / ATAN_SATURATION;
+    let mut z = (x.sin() + p.t * PI * (y + p2.z).cos() + s.c.powf(E) + (E * s.c.cos() * (SQRT_2 * y).cos()) + s.c3.sqrt() * s.c2.cos()).atan() / ATAN_SATURATION;
+    if x.is_nan() { x = p2.x };
+    if y.is_nan() { y = p2.y };
+    if z.is_nan() { z = p2.z };
+    Point { x: x, y: y, z: z }
+}
+
+fn equation_007(s: &State, _p: &Parameter, p1: &Point, p2: &Point) -> Point {
+    let mut x = ((s.c - s.n).sin() + (s.c3 + s.p1.y).cos() * p1.z).atan() / ATAN_SATURATION;
+    let mut y = (s.c.cos() + (s.c2 * p2.x).sin() * p1.x * p2.z + (s.p1.z.abs() + p2.z.abs()).sqrt()).atan() / ATAN_SATURATION;
+    let mut z = (s.c2.sinh() + s.c3.tanh()).atan() / ATAN_SATURATION;
+    if x.is_nan() { x = p2.x };
+    if y.is_nan() { y = p2.y };
+    if z.is_nan() { z = p2.z };
+    Point { x: x, y: y, z: z }
+}
+
+
+fn select_equation(index: usize) -> fn(&State, &Parameter, p1: &Point, p2: &Point) -> Point {
     match index {
         1 => equation_001,
         2 => equation_002,
         3 => equation_003,
         4 => equation_004,
         5 => equation_005,
+        6 => equation_006,
+        7 => equation_007,
         _ => equation_000,
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
-/*
-pub fn new_x_001(cs: f64, x: u32, y: u32, z:f64, c:f64) -> u32 {
-    (cx + c.sin() * r + (c * 1.777).cos() * z * (imgy as f64)).round() as u32
-}
-
-pub fn new_y_001(cs: f64, x: u32, y: u32, z:f64, c:f64) -> u32 {
-    (cy + c.cos() * r + (c * 1.666).sin() * ((x as f64 * z * (imgx as f64))).sqrt() as f64).round() as u32
-}
-*/
-
 fn min_max(samples: &[i32]) -> (i32, i32) {
     let min = *samples.iter().min().unwrap_or(&0);
     let max = *samples.iter().max().unwrap_or(&0);
